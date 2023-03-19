767195451bd9dbe9986345afa91753f94ff1139d
Paolo <paolo.pellegrinotti@mail.polimi.it>
2023-03-19 16:16:02 +0000

    trying to understand something...

---
+++ CNSolver.cpp
@@ -0,0 +1,19 @@
+#include "CNSolver.hpp"
+
+
+std::tuple<std::vector<double>, std::vector<double>> CNSolver(const std::function<double(double,double)>& f, const double & y0, const double & T, const int & N){
+	const double h = T/N;
+	std::vector<double> u(N+1);
+	std::vector<double> t(N+1);
+	u[0] = y0;
+	t[0] = 0;
+	for(auto i=0; i <= N; i++){
+		double t_i = h*i;
+		t.push_back(t_i);
+		u.push_back(bisection([&f,h,t_i](double x) {return (x-(h/2.)*(f(t_i+h,x)+f(t_i,x)));}, 0, T));
+	
+	}
+
+
+}
+
---
+++ CNSolver.hpp
@@ -0,0 +1,7 @@
+#include <tuple>
+#include <functional>
+#include <vector>
+#include "/home/p2/PACS/pacs-examples/Examples/src/LinearAlgebraUtil/basicZeroFun.hpp"
+
+
+std::tuple<std::vector<double>, std::vector<double>> CNSolver(const std::function<double(double,double)>& f, const double & y0, const double & T, const double & N);
