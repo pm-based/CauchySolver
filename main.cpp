939f26ef1b67929cf7cb29ca6a326c9964448ce3
Paolo <paolo.pellegrinotti@mail.polimi.it>
2023-03-20 06:48:43 +0000

    write main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "CNSolver.hpp"
-#include <<numbers>
+//#include <functional>
+//#include <numbers>
 
 int main(){
-	fun = [](double y, double t){return -t*(numbers::e)^(-y);
+	std::function<double(double,double)> fun = [](double t, double y){return -t*exp(-y);};
+	double y0 = 0;
+	int N = 100;
+	double T = 1;
+	auto p = CNSolver(fun, y0, T, N);
 
 
 }
