77df77a296a413887a9a03693324d83bb067b50f
Paolo <paolo.pellegrinotti@mail.polimi.it>
2023-03-23 14:58:04 +0000

    clean up and using the new method CauchyProblem::plot()

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include "CNSolver.hpp"
-#include "gnuplot-iostream.hpp" // interface with gnuplot
-#include "cauchyProblem.hpp"
+#include <cmath>
+#include "cauchyProblem.hpp"	// Class to handle Cauchy problems
+//#include "gnuplot-iostream.hpp"
+
+using ODE = std::function<double(double,double)>;
 
 int main(){
-	std::function<double(double,double)> fun = [](double t, double y){return -t*exp(-y);};
-	//std::function<double(double,double)> fun = [](double t, double y){return 4*y;};
-	double y0 = 0;
-	int N = 100;
-	double T = 1;
-	auto p = CNSolver(fun, y0, T, N);
+	// ODE of the challenge
+	const ODE ChallengeOde = [](double t, double y){return -t*exp(-y);}; 
 	
-	CauchyProblem ChallengeProblem(fun);
+	//Other ODE's
+	const ODE odeTest1 = [](double t, double y){return 2*y;};
+	const ODE odeTest2 = [](double t, double y){return cos(t) + y/tan(t);};
 	
-	Gnuplot gp; // gnuplot iostream
-	gp << "plot" << gp.file1d(ChallengeProblem.CNSolution()) << std::endl;
-
+	//Declaration and initialization of Cauchy problems
+	CauchyProblem ChallengeProblem(ChallengeOde);
+	
+	CauchyProblem TestCP(odeTest1, 2, 1.2, 3);
+	TestCP.SetNumberOfSteps(1000);
+	
+	ChallengeProblem.CNSolution();
+	ChallengeProblem.plot();
+	
+	TestCP.CNSolution();
+	TestCP.plot();
+	
+	
 	return(0);
 }
 
