e2bea27f28a833cee431d60546c89cd9c42ffc09
Paolo <paolo.pellegrinotti@mail.polimi.it>
2023-03-23 10:11:57 +0000

    reordering code and implement a method to set the time steps (and delete the definition of time step in the constructor)

--- cauchyProblem.hpp
+++ cauchyProblem.hpp
@@ -3,18 +3,19 @@
 #include <functional>
 #include "CNSolver.hpp"
 
-class CauchyProblem {
-	using ODE = std::function<double(double,double)>;
-	using NumSolution = std::tuple<std::vector<double>, std::vector<double>>;
-	
+using ODE = std::function<double(double,double)>;
+using NumSolution = std::tuple<std::vector<double>, std::vector<double>>;
+
+
+//Class that define a Cauchy problem, and implement the methods to get the solution
+class CauchyProblem {	
 	public:
 		//a complete constructor: full definition of a cauchy problem and of the steps to compute it
-		CauchyProblem(ODE f, double y0, double t0, double t1, int N):
+		CauchyProblem(ODE f, double y0, double t0, double t1):
 			odefun(f),
 			InitialValue(y0),
 			InitialTime(t0),
-			EndTime(t1),
-			Nsteps(N)
+			EndTime(t1)
 			{};
 		
 		//constructor only with the specified ode function
@@ -25,15 +26,18 @@ class CauchyProblem {
 		NumSolution CNSolution(){
 			return CNSolver(odefun, InitialValue, InitialTime, EndTime, 					Nsteps);};
 		
+		//method to set a costumize number of steps
+		void SetNumberOfSteps(const int & N){Nsteps = N;};
+		
 	private:
-		// definition of the cauchy problem
+		// definition of the cauchy problem. They are private since characterize  the cauchy problem. If they could be changed then it would be a different cauchy problem.
 		const ODE odefun; 
 		const double InitialValue = 0;
 		const double InitialTime = 0;
 		const double EndTime = 1;
 		
 		// definition of solvig parameter
-		const int Nsteps = 100;	//Number of steps
+		int Nsteps = 100;	//Number of steps
 		
 		// Solution
 		NumSolution sol;
